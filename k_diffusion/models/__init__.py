06261e9e76e8e3bd7be283667a2596d5e3c3c9f5
Nyries <cyprien.gaborieau@gmail.com>
2024-08-20 11:21:58 +0200

    __init__ file

--- k_diffusion/models/__init__.py
+++ k_diffusion/models/__init__.py
@@ -3,3 +3,5 @@ from .flags import checkpointing, get_checkpointing
 from .image_v1 import ImageDenoiserModelV1
 from .image_transformer_v1 import ImageTransformerDenoiserModelV1
 from .image_transformer_v2 import ImageTransformerDenoiserModelV2
+from .configA import ConfigADenoiser
+from .configB import ConfigBDenoiser
